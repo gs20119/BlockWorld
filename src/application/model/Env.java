5650d9cd1f53eb1ece3744362d715e25d2c38a14
gs20119 <gs20119@gs.hs.kr>
2021-06-16 11:10:16 +0900

    Implemented Moving + Save Track Mechanism

--- src/application/model/Env.java
+++ src/application/model/Env.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class Env {
 	private int[] Board;
-	private int[][] mvIdx = new int[4][];
+	private int[][] mvIdx;
 	private int size=4, cap=16;
 	private int time=0;
 	private static int Max=10, genMax=3;
@@ -16,12 +16,13 @@ public class Env {
 		this.size=size; 
 		this.cap=size*size;
 		Board = new int[this.cap];
-		for(int i=0; i<4; i++) mvIdx[i] = new int[this.cap];
+		mvIdx = new int[this.size][];
+		for(int i=0; i<this.size; i++) mvIdx[i] = new int[this.cap];
 		for(int j=0; j<this.cap; j++) mvIdx[0][j] = j; // left
 		for(int j=0; j<this.cap; j++) mvIdx[1][j] = size*(j%size) + ((int)j/size); // up
 		for(int j=0; j<this.cap; j++) mvIdx[2][j] = j + (size-1) - 2*(j%size); // right
 		for(int j=0; j<this.cap; j++) mvIdx[3][j] = size*(size-1-j%size) + ((int)j/size); // down
-		for(int j=0; j<this.cap; j++) System.out.print(mvIdx[3]);
+		for(int j=0; j<this.cap; j++) System.out.print(mvIdx[3][j]);
 		System.out.println("");
 		Generate();
 	}
@@ -42,18 +43,24 @@ public class Env {
 		Board[Randi] = Randv;
 	}
 	
-	private int[] Eval(int[] Cur) { // 0 방향으로 밀기
-		int[] Mv = new int[size];
-		int top=-1, topv=0;
-		for(int i=0; i<size; i++) {
-			if(Cur[i]==0) continue;
-			if(topv==Cur[i]) { Mv[i]=i-top; top++; topv=0; }
-			else { top++; Mv[i]=i-top; topv=Cur[i]; }
-		}return Mv;
-	}
-	
-	public void Move(int action) {
-		
+	private void Move(int act) { // 0 방향으로 밀기
+		int[] Mv = new int[cap];
+		for(int j=0; j<size; j++) {
+			int top=0, topv=0;
+			for(int i=0; i<size; i++) {
+				int here = mvIdx[act][i+j*size];
+				if(Board[here]==0) continue;
+				if(topv==Board[here]) { 
+					Mv[here]=i-top; topv++;
+					Board[mvIdx[act][top+j*size]]=topv;
+					top++; topv=0; 
+				}else { 
+					if(topv!=0) { Board[mvIdx[act][top+j*size]]=topv; top++; }
+					Mv[here]=i-top; topv=Board[here]; Board[here]=0;
+				}
+			}
+		}Generate();
+		Show();
 	}
 	
 	public int[] getState() { return Board; }
